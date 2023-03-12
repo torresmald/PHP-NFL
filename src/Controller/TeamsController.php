6f97ea23b7d1f3d22003be6cdcba11cd96d4223d
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-03-12 22:53:41 +0100

    feat added listTeams

--- src/Controller/TeamsController.php
+++ src/Controller/TeamsController.php
@@ -5,15 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TeamsController extends AbstractController{
-    #[Route('/teams')]
-    public function showTeam(){
+class TeamsController extends AbstractController
+{
+    #[Route('/team')]
+    public function showTeam()
+    {
         $team = [
+            'code' => '1',
             'name' => 'KC Chiefs',
             'city' => 'Kansas City',
+            'logo' => 'https://static.www.nfl.com/image/private/f_auto/league/u9fltoslqdsyao8cpm0k',
             'established' => 1920,
-            'head coach' => 'Jonathan Gannon'
+            'headCoach' => 'Jonathan Gannon'
         ];
         return $this->render('teams/showTeams.html.twig', ['team' => $team]);
+    }
+    #[Route('/teams', name: 'listTeams')]
+    public function listTeams()
+    {
+        $teams =
+            [
+                [
+                    'code' => '1',
+                    'name' => 'KC Chiefs',
+                    'city' => 'Kansas City',
+                    'logo' => 'https://static.www.nfl.com/image/private/f_auto/league/u9fltoslqdsyao8cpm0k',
+                    'established' => 1920,
+                    'headCoach' => 'Jonathan Gannon'
+                ],
+                [
+                    'code' => '2',
+                    'name' => 'New England Patriots',
+                    'city' => 'Foxborough',
+                    'logo' => 'https://static.www.nfl.com/image/private/f_auto/league/u9fltoslqdsyao8cpm0k',
+                    'established' => 1920,
+                    'headCoach' => 'Jonathan Gannon'
+                ]
+            ];
+
+        return $this->render('teams/listTeams.html.twig', ['teams' => $teams]);
     }
 }
